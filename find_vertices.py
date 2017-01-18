83d7b914f8c1cea99277ea4aedbc7644c4db99d0
Steve Mussmann <smussmann@gmail.com>
2017-01-18 10:27:20 -0600

    Minor cleanup of code, allow for changing of locality code field name

--- find_vertices.py
+++ find_vertices.py
@@ -1,12 +1,11 @@
 from __future__ import print_function
 import arcpy
 import os
-import sys
 from comline import ComLine
 
 class FindVertices():
 	'Class for finding vertices corresponding to sampling sites and stream intersections in an ESRI shapefile containing lines representing interconnected streams'
-	def __init__(self,points,streams):
+	def __init__(self,points,streams,code):
 		# set workspace
 		arcpy.env.workspace = os.getcwd()
 		arcpy.env.overwriteOutput=True
@@ -15,13 +14,13 @@ class FindVertices():
 		
 		dissolved = self.dissolve_lines(streams, "streams_dissolve.shp") #dissolve a streams layer with many line segments
 		self.snap_points(points, dissolved)#snap points to the streams to ensure they will accurately split the lines
-		splits = self.split_line(points,dissolved,"split_streams.shp") #split all lines at the sampling sites
-		endpoints = self.end_points(splits,"end_points.shp") #make endpoints for resulting split streams from previous command
+		self.splits = self.split_line(points,dissolved,"split_streams.shp") #split all lines at the sampling sites
+		endpoints = self.end_points(self.splits,"end_points.shp") #make endpoints for resulting split streams from previous command
 		self.add_xy(endpoints) #add XY cooridnates to attribute table of the endpoints that were created for all lines
 		self.add_xy(points) #add XY coordinates to attribute table of sampling sites
 		self.remove_identical(endpoints) #remove redundant points at ends of stream segments
 		erased = self.erase_points(endpoints,points,"end_points_dissolve_erase.shp")
-		self.vertices = self.merge_points(erased,points,"all_vertices.shp")
+		self.vertices = self.merge_points(erased,points,"all_vertices.shp",code)
 
 	def dissolve_lines(self,streams,out):
 		print("Dissolving input streams file", streams)
@@ -62,34 +61,14 @@ class FindVertices():
 		arcpy.Erase_analysis(endpoints,points,out,"1 Meters")
 		return out
 	
-	def merge_points(self,erased,points,out):
+	def merge_points(self,erased,points,out,code):
 		print("Merging into final output")
 		print("")
 		fieldMappings = arcpy.FieldMappings()#create fieldmapping object
 		fieldMappings.addTable(erased)#get fields from erased
 		fieldMappings.addTable(points)#get fields from points
 		for field in fieldMappings.fields:
-			if field.name not in ["POINT_X","POINT_Y","CODE"]:
+			if field.name not in ["POINT_X","POINT_Y",code]:
 				fieldMappings.removeFieldMap(fieldMappings.findFieldMapIndex(field.name))
 		arcpy.Merge_management([erased,points],out,fieldMappings)
 		return out
-	
-#def main():
-#	#obj = ComLine(sys.argv[1:])
-#	streams = obj.args.streams
-#	points = obj.args.points
-
-	#dissolved = dissolve_lines(streams, "streams_dissolve.shp") #dissolve a streams layer with many line segments
-	#snap_points(points, dissolved)#snap points to the streams to ensure they will accurately split the lines
-	#splits = split_line(points,dissolved,"split_streams.shp") #split all lines at the sampling sites
-	#endpoints = end_points(splits,"end_points.shp") #make endpoints for resulting split streams from previous command
-	#add_xy(endpoints) #add XY cooridnates to attribute table of the endpoints that were created for all lines
-	#add_xy(points) #add XY coordinates to attribute table of sampling sites
-	#remove_identical(endpoints) #remove redundant points at ends of stream segments
-	#erased = erase_points(endpoints,points,"end_points_dissolve_erase.shp")
-	#merge_points(erased,points,"all_vertices.shp")
-		
-
-#main()
-		
-#raise SystemExit
