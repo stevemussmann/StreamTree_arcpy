92fcb629211cc109123e4ee337efca4491fee9d6
Steve Mussmann <smussmann@gmail.com>
2017-01-18 10:28:10 -0600

    include FindLines class

--- streamtree_arcpy.py
+++ streamtree_arcpy.py
@@ -2,12 +2,15 @@
 
 from comline import ComLine
 from find_vertices import FindVertices
+from getlines import FindLines
 import sys
 
 def main():
 	input = ComLine(sys.argv[1:])
-	verts = FindVertices(input.args.points, input.args.streams)
+	verts = FindVertices(input.args.points, input.args.streams,input.args.code)
 	print verts.vertices
+	print verts.splits
+	lines = FindLines(verts.vertices,verts.splits)
 	
 main()
 
